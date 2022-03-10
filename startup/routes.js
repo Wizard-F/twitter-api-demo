75dc009da9fa006d6d95c56997e75b52594c15c1
Wizard-F <forrest.futao.wei@gmail.com>
2022-03-10 21:43:13 +0800

    added cors

--- startup/routes.js
+++ startup/routes.js
@@ -1,10 +1,12 @@
 const express = require('express')
+const cors = require('cors')
 
 const users = require('../routes/users')
 const tweets = require('../routes/tweets')
 const auth = require('../routes/auth')
 
 module.exports = function (app) {
+  app.use(cors())
   app.use(express.json())
   app.use('/api/users', users)
   app.use('/api/tweets', tweets)
