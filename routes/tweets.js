af242c4b720a0e60b08e4721a3c78db159468793
Wizard-F <forrest.futao.wei@gmail.com>
2022-03-20 14:24:55 +0800

    added delete-tweet functionality

--- routes/tweets.js
+++ routes/tweets.js
@@ -57,4 +57,16 @@ router.put('/:id', auth, async (req, res) => {
   res.send(tweet)
 })
 
+router.delete('/:id', auth, async (req, res) => {
+  const tweet = await Tweet.findById(req.params.id)
+  if (!tweet) {
+    return res.status(404).send('Tweet not found!')
+  }
+  if (req.user._id.toString() !== tweet.author.toString()) {
+    return res.status(403).send('Permission denied!')
+  }
+  await Tweet.findByIdAndDelete(req.params.id)
+  res.send(tweet)
+})
+
 module.exports = router
